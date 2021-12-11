c73f6f5b1c4d63524d66d9f062ba2ac9c9fc3603
Kangbo <kangbo.io@icloud.com>
2021-12-11 13:45:06 -0500

    Style refactoring

--- src/Vanilla.jl
+++ src/Vanilla.jl
@@ -5,21 +5,20 @@ export scdm_condense_phase
 
 Perform SCDM for condense phase orbitals.
 The `bands` amongst the orbitals `u::Wannier` will be localized.
+The rest of the gauge will be an identity matrix.
 """
-function scdm_condense_phase(wannier::Wannier{UnkBasisOrbital{T}}, bands::AbstractVector{<:Integer}, ortho=true) where T <: HomeCell
-    brillouin_zone = grid(wannier)
-    gamma_point = brillouin_zone[0, 0, 0]
-    homecell = grid(wannier[gamma_point][1])
-
-    vectorize(o::UnkBasisOrbital{T}) = reshape(elements(o), prod(size(grid(o))))
+function scdm_condense_phase(u::Wannier{UnkBasisOrbital{T}}, bands::AbstractVector{<:Integer}, ortho=true) where T <: HomeCell
+    brillouin_zone = grid(u)
+    Γ = brillouin_zone[0, 0, 0]
+    homecell = grid(u[Γ][1])
 
     columns = begin
-        Ψ_Γ = hcat(vectorize.(wannier[gamma_point][bands])...)
+        Ψ_Γ = hcat(vectorize.(u[Γ][bands])...)
         F = qr(Ψ_Γ', ColumnNorm())
         F.p[1:length(bands)]
     end
 
-    n_bands_complete = length(elements(wannier)[1])
+    n_bands_complete = length(elements(u)[1])
     U = Gauge(brillouin_zone, n_bands_complete)
 
     orthonormalize(A::AbstractMatrix) = let (U, _, V) = svd(A)
@@ -27,8 +26,8 @@ function scdm_condense_phase(wannier::Wannier{UnkBasisOrbital{T}}, bands::Abstra
     end
 
     for k in collect(brillouin_zone)
-        phase = (r->exp(1im * (r' * k))).(homecell(columns))
-        Ψ = hcat(vectorize.(wannier[k][bands])...)
+        phase = (r->exp(1im * (k' * r))).(homecell(columns))
+        Ψ = hcat(vectorize.(u[k][bands])...)
         Ψ = diagm(phase) * Ψ[columns, :]
         Ψ = Ψ'
 
