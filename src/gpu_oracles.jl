c1e0d1d5bd4c5333274a6c6ccfc3124f829b27b5
Kangbo <kangbo.io@icloud.com>
2024-05-09 19:19:57 -0400

    reduction kernel

--- src/gpu_oracles.jl
+++ src/gpu_oracles.jl
@@ -1,79 +1,164 @@
+using LinearAlgebra
+using CUDA
+export GPUOracleF, GPUOracleGradF, make_f_gpu, make_grad_f_gpu, retract_gpu!
 
-export OracleF, OracleGradF, make_f, make_grad_f, retract!
-
-struct OracleF
-    s::Array{ComplexF64,4}
+struct GPUOracleF
+    s::CuArray{ComplexF64,4}
     w_list::Vector{Float64}
-    k_plus_b::Matrix{Int64}
+    k_plus_b::CuArray{Int64,2}
     n_k::Int64
     n_b::Int64
     n_e::Int64
     n_j::Int64
-    r::Array{ComplexF64,4}
-    rho_hat::Matrix{ComplexF64}
+    r::CuArray{ComplexF64,4}
+    rho_hat::CuArray{ComplexF64,2}
     omega::Vector{Float64}
-    m_work::Array{ComplexF64,3}
+    m_work::CuArray{ComplexF64,3}
 end
 
-function make_f(s::Array{ComplexF64,4}, w_list, kplusb, n_k, n_b, n_e, n_j)
-    r = zeros(ComplexF64, size(s))
-    rho_hat = zeros(ComplexF64, n_e, n_b)
+function make_f_gpu(s, w_list, kplusb, n_k, n_b, n_e, n_j)
+    r = CUDA.zeros(ComplexF64, size(s))
+    rho_hat = CUDA.zeros(ComplexF64, n_e, n_b)
     omega = zeros(Float64, n_b)
-    m_work = zeros(ComplexF64, n_e, n_k, n_b)
-    OracleF(s, w_list, kplusb, n_k, n_b, n_e, n_j, r, rho_hat, omega, m_work)
+    m_work = CUDA.zeros(ComplexF64, n_e, n_k, n_b)
+    GPUOracleF(s, w_list, kplusb, n_k, n_b, n_e, n_j, r, rho_hat, omega, m_work)
 end
 
-function (f::OracleF)(u::Array{ComplexF64,3})
-    #= f_oracle!(f.STensor, f.r, UTensor, f.w_list, f.kplusb, f.rho_hat, f.n_k, f.n_b, f.n_e, f.Nj, f.omega) =#
-    #= LinearAlgebra.BLAS.gemm!('N', 'N', one, f.STensor[:, :, k, b], UTensor[:, :, f.kplusb[k, b]], zero, view(f.r, :, :, k, b)) =#
-    #= rho_hat_handle = reinterpret(Float64, f.rho_hat) =#
+function f_kernel_1(s, r, u, k_plus_b, n_k, n_b, n_e)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+    for i in index:stride:(n_k*n_e)
+        k = div(i - 1, n_e) + 1
+        p = mod(i - 1, n_e) + 1
+        #= end
+        for k in index:stride:n_k
+            for p in 1:n_e =#
+        for z in 1:n_e
+            for b in 1:n_b
+                for q in 1:n_e
+                    r[p, q, k, b] += s[p, z, k, b] * u[z, q, k_plus_b[k, b]]
+                end
+            end
+            #= end =#
+        end
+    end
+
+end
+
+function f_kernel_2(r, u, m_work, n_k, n_b, n_e)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+
+    for i in index:stride:(n_k*n_e)
+        k = div(i - 1, n_e) + 1
+        n = mod(i - 1, n_e) + 1
+        #= for k in index:stride:n_k
+            for n in 1:n_e =#
+        for b in 1:n_b
+            for i in 1:n_e
+                m_work[n, k, b] += u[i, n, k]' * r[i, n, k, b]
+            end
+        end
+    end
+    #= end =#
+end
+
+#= function f_kernel_3(rho_hat, m_work, n_k, n_b, n_e)
+    for n in 1:n_e
+        for b in 1:n_b
+            for k in 1:n_k
+                rho_hat[n, b] += m_work[n, k, b]
+            end
+        end
+    end
+end =#
+
+function f_kernel_sum(data)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+    s = 1
+    while s < length(data)
+        for i in index:stride:div(length(data) + 1, 2)
+            idx = (i - 1) * 2s + 1
+            if idx + s <= length(data)
+                data[idx] += data[idx+s]
+            end
+        end
+        sync_threads()
+        s *= 2
+    end
+end
+
+function f_kernel_3(data, n_k, n_b, n_e)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+    s = 1
+    n_be = n_b * n_e
+    k_stride = div(stride, n_be)
+    if index > k_stride * n_be
+        return 
+    end
+    k_half = div(n_k + 1, 2)
+    k_index = div(index - 1, n_be) + 1
+    bn = rem(index-1, n_be) + 1
+    b = div(bn-1, n_e) + 1
+    n = rem(index-1, n_e) + 1
+
+    while s < n_k
+        for i in k_index:k_stride:k_half
+            idx = (i - 1) * 2s + 1
+            if idx + s <= n_k
+                data[n, idx, b] += data[n, idx+s, b]
+            end
+        end
+        sync_threads()
+        s *= 2
+    end
+end
+
+
+function (f::GPUOracleF)(u)
     fill!(f.rho_hat, 0)
     fill!(f.m_work, 0)
     fill!(f.r, 0)
-    BLAS.set_num_threads(1)
-    @time Threads.@threads for k in 1:f.n_k
-        #= @time for k in 1:f.n_k =#
+    @time @cuda threads = 256 f_kernel_1(f.s, f.r, u, f.k_plus_b, f.n_k, f.n_b, f.n_e)
+    @time @cuda threads = 256 f_kernel_2(f.r, u, f.m_work, f.n_k, f.n_b, f.n_e)
+    #= for k in 1:f.n_k
         for b in 1:f.n_b
-            #= for z in 1:f.n_e
-                for p in 1:f.n_e
-                    for q in 1:f.n_e
-                        @inbounds f.r[p, q, k, b] += f.s[p, z, k, b] * u[z, q, f.k_plus_b[k, b]]
-                    end
-                end
-            end =#
-            @inbounds mul!(view(f.r, :, :, k, b), view(f.s, :, :, k, b), view(u, :, :, f.k_plus_b[k, b]))
+            mul!(view(f.r, :, :, k, b), view(f.s, :, :, k, b), view(u, :, :, f.k_plus_b[k, b]))
         end
-    end
-    BLAS.set_num_threads(Threads.nthreads)
-
-    for k in 1:f.n_k
         for n in 1:f.n_e
             for b in 1:f.n_b
-                @inbounds f.m_work[n, k, b] = dot(view(u, :, n, k), view(f.r, :, n, k, b))
-                #= for i in 1:f.n_e
-                    @inbounds f.m_work[n, k, b] += conj(u[i, n, k]) * f.r[i, n, k, b]
-                end =#
+                f.m_work[n, k, b] = dot(view(u, :, n, k), view(f.r, :, n, k, b))
             end
         end
-    end
+    end =#
 
-    #= rho_hat_handle = reinterpret(Float64, f.rho_hat)
-    m_work_handle = reinterpret(Float64, f.m_work) =#
-    for k in 1:f.n_k
-        for n in 1:f.n_e
-            #= n1, n2 = 2n - 1, 2n =#
+    #= p = CUDA.ones(Float64, 27)
+    @cuda f_kernel_sum(p)
+    println(p) =#
+
+    #= @time for n in 1:f.n_e
+        for b in 1:f.n_b
+            @cuda f_kernel_sum(view(f.m_work, n, :, b))
+        end
+    end
+    copyto!(f.rho_hat, view(f.m_work, :, 1, :)) =#
+    #
+    #= @time copyto!(f.rho_hat, sum(f.m_work, dims=2)) =#
+    @time @cuda threads=256 f_kernel_3(f.m_work, f.n_k, f.n_b, f.n_e)
+    copyto!(f.rho_hat, view(f.m_work, :, 1, :))
+
+    #= for n in 1:f.n_e
+        for k in 1:f.n_k
             for b in 1:f.n_b
-                @inbounds f.rho_hat[n, b] += f.m_work[n, k, b]
-                #= @inbounds rho_hat_handle[n1, b] += m_work_handle[n1, b]
-                @inbounds rho_hat_handle[n2, b] += m_work_handle[n2, b] =#
+                f.rho_hat[n, b] += f.m_work[n, k, b]
             end
         end
-    end
-    lmul!(1 / f.n_k, f.rho_hat)
-    #= map!(abs, view()f.m_work, f.rho_hat) =#
-    #= axpby!(1 / f.n_k, f.m_work, 0, f.rho_hat) =#
+    end =#
+    @time lmul!(1 / f.n_k, f.rho_hat)
 
-    for b in 1:f.n_b
+    @time for b in 1:f.n_b
         #= f.omega[b] = 2 * f.w_list[b] * (f.n_e - sum(real, view(f.m_work, :, b))) =#
         f.omega[b] = 2 * f.w_list[b] * (f.n_e - sum(abs.(view(f.rho_hat, :, b))))
     end
@@ -81,58 +166,68 @@ function (f::OracleF)(u::Array{ComplexF64,3})
     return sum(f.omega)
 end
 
-struct OracleGradF
-    f::OracleF
+struct GPUOracleGradF
+    f::GPUOracleF
     grad_work::Matrix{ComplexF64}
     grad_omega::Array{ComplexF64,3}
 end
 
-function make_grad_f(f::OracleF)
+function make_grad_f_gpu(f::GPUOracleF)
     grad_work = zeros(ComplexF64, f.n_e, f.n_e)
     grad_omega = zeros(ComplexF64, f.n_e, f.n_e, f.n_k)
-    return OracleGradF(f, grad_work, grad_omega)
+    return GPUOracleGradF(f, grad_work, grad_omega)
 end
 
-function (grad_f::OracleGradF)(u::Array{ComplexF64,3})
+function anti_symmetrize_gpu!(dst, src)
+    for i in axes(src, 1)
+        for j in axes(src, 2)
+            dst[i, j] = (src[i, j] - src[j, i]') / 2
+        end
+    end
+    return dst
+end
+
+function (grad_f::GPUOracleGradF)(u::Array{ComplexF64,3})
     f = grad_f.f
     #= grad_f_oracle!(f.r, f.w_list, f.rho_hat, f.n_k, f.n_b, f.n_e, f.Nj, grad_f.grad_omega) =#
     fill!(grad_f.grad_omega, 0)
-    copy!(f.rho_hat, conj.(f.rho_hat) ./ abs.(f.rho_hat))
+    map!(abs, view(f.m_work, :, :, 1), f.rho_hat)
+    map!(conj, f.rho_hat, f.rho_hat)
+    map!(/, f.rho_hat, f.rho_hat, view(f.m_work, :, :, 1))
 
     for b in 1:f.n_b
         rmul!(view(f.rho_hat, :, b), f.w_list[b])
-        #= ] = f.rho_hat[:, b] * f.w_list[b] =#
-        for n = 1:f.n_e
-            axpy!(f.rho_hat[n, b], view(f.r, :, n, :, b), view(grad_f.grad_omega, :, n, :))
+    end
+
+    Threads.@threads for k in 1:f.n_k
+        for b in 1:f.n_b
+            #= ] = f.rho_hat[:, b] * f.w_list[b] =#
+            for n = 1:f.n_e
+                axpy!(f.rho_hat[n, b], view(f.r, :, n, k, b), view(grad_f.grad_omega, :, n, k))
+            end
         end
     end
     #= LinearAlgebra.axpy!((-2 / f.n_k), grad_f.grad_omega, grad_f.grad_omega) =#
-    lmul!(-2 / f.n_k, grad_f.grad_omega)
 
     #= SCDM.project!(UTensor, grad_f.grad_omega, grad_f.grad_work, f.n_k, f.n_e) =#
-    for k in 1:f.n_k
+    Threads.@threads for k in 1:f.n_k
+        lmul!(-2 / f.n_k, view(grad_f.grad_omega, :, :, k))
         #= LinearAlgebra.BLAS.gemm!('C', 'N', ComplexF64(1), view(UTensor, :, :, k),
             view(grad_f.grad_omega, :, :, k), ComplexF64(0), grad_f.grad_work)
         grad_f.grad_omega[:, :, k] = grad_f.grad_work - grad_f.grad_work' =#
 
-        BLAS.gemm!('C', 'N', ComplexF64(1), view(u, :, :, k), view(grad_f.grad_omega, :, :, k), ComplexF64(0), grad_f.grad_work)
-        mul!(view(grad_f.grad_omega, :, :, k), view(u, :, :, k), (grad_f.grad_work - grad_f.grad_work') / 2)
+        BLAS.gemm!('C', 'N', ComplexF64(1), view(u, :, :, k), view(grad_f.grad_omega, :, :, k), ComplexF64(0), view(f.r, :, :, k, 1))
+        mul!(view(grad_f.grad_omega, :, :, k), view(u, :, :, k), anti_symmetrize!(view(f.r, :, :, k, 2), view(f.r, :, :, k, 1)))
+        #= mul!(view(grad_f.grad_omega, :, :, k), view(u, :, :, k), (view(f.r, :, :, k, 1) - view(f.r, :, :, k, 1)')/2) =#
     end
     return grad_f.grad_omega
 end
 
-struct SVDRetraction end
-struct QRRetraction end
-struct ExpRetraction end
-
-function retract!(u_buffer, u, d_u, t, ::QRRetraction)
+function retract_gpu!(u_buffer, u, d_u, t, ::QRRetraction)
     n_e, _, n_k = size(u)
     copy!(u_buffer, u)
-    axpy!(t, d_u, u_buffer)
-    #= time_1 = time_ns() =#
-    #= qrfacs = [qr!(u_buffer[:, :, k]) for k = 1:n_k] =#
-    #= time_2 = time_ns() =#
-    for k in 1:n_k
+    Threads.@threads for k in 1:n_k
+        axpy!(t, view(d_u, :, :, k), view(u_buffer, :, :, k))
         for p = 1:n_e
             norm_p = norm(view(u_buffer, :, p, k))
             lmul!(1 / norm_p, view(u_buffer, :, p, k))
@@ -141,16 +236,10 @@ function retract!(u_buffer, u, d_u, t, ::QRRetraction)
                 axpy!(-r, view(u_buffer, :, p, k), view(u_buffer, :, q, k))
             end
         end
-        #= d = diag(qrfacs[k].R)
-        D = Diagonal(sign.(sign.(d .+ 1 // 2)))
-        mul!(view(u_buffer, :, :, k), Matrix(qrfacs[k].Q), D) =#
     end
-    #= time_3 = time_ns()
-    println("qr time: ", time_2 - time_1)
-    println("mul time: ", time_3 - time_2) =#
 end
 
-function retract!(u_buffer, u, d_u, t, ::SVDRetraction)
+function retract_gpu!(u_buffer, u, d_u, t, ::SVDRetraction)
     Nk = size(u, 3)
     copy!(u_buffer, u)
     axpy!(t, d_u, u_buffer)
@@ -160,7 +249,7 @@ function retract!(u_buffer, u, d_u, t, ::SVDRetraction)
     end
 end
 
-function retract!(u_buffer, u, d_u, t, ::ExpRetraction)
+function retract_gpu!(u_buffer, u, d_u, t, ::ExpRetraction)
     Nk = size(u, 3)
 
     for k in 1:Nk
@@ -170,8 +259,3 @@ function retract!(u_buffer, u, d_u, t, ::ExpRetraction)
     end
 end
 
-    #= for n in 1:f.n_e
-        for b in 1:f.n_b
-            @inbounds f.rho_hat[n, b] = dot(view(u, :, n, :), view(f.r, :, n, :, b))
-        end
-    end =#
---
+++ test/gpu_cg.jl
@@ -0,0 +1,24 @@
+using SCDM, LinearAlgebra, Random, Profile
+
+src_dir = "./test/scdm_dataset/BaTiO3"
+s, u, w_list, k_plus_b, n_k, n_b, n_e = load_problem(src_dir)
+u = CuArray(u)
+
+f_gpu = make_f_gpu(s, w_list, k_plus_b, n_k, n_b, n_e, n_e)
+grad_f_gpu = make_grad_f_gpu(f_gpu)
+
+Random.seed!(16)
+u = CUDA.zeros(ComplexF64, n_e, n_e, n_k)
+
+for k in 1:n_k
+    a = rand(n_e, n_e)
+    u[:, :, k] = let (u, _, v) = svd(a)
+        u * v'
+    end
+end
+
+@time f_gpu(u)
+
+@time cg(u, f_gpu, grad_f_gpu, retract!, n_e);
+@time grad_f(u);
+@profview cg(u, f, grad_f, n_e, n_k);
